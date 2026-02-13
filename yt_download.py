ce5c3653a728f7ec076436661fd0860f21b20378
Sujoy Das <sujoydas1700@gmail.com>
2026-02-13 23:07:06 +0530

    initial commit and this is main code to download the youtube video

---
+++ yt_download.py
@@ -0,0 +1,27 @@
+import yt_dlp
+
+def download(url,ydl_opts):
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(url)
+        print('done')
+    except Exception as e:
+        print(e)
+
+class yt_dld:
+    def __init__(self,url):
+        self.url = url
+
+    def video_480p(self):
+        ydl_opts = {
+            'format': 'best[ext=mp4][height<=480]',
+            'outtmpl': '%(title)s_480p.%(ext)s',
+        }
+        download(self.url,ydl_opts)
+    
+    def video_720(self):
+        ydl_opts = {
+            'format': 'best[ext=mp4][height<=720]',
+            'outtmpl': '%(title)s_720p.%(ext)s',
+        }
+        download(self.url,ydl_opts)
